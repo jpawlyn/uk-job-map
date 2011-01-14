f40d5c05384751a85d5d0ee7b8452b88ee0aedb8
Jolyon Pawlyn <jolyon.pawlyn@unboxedconsulting.com>
2011-01-14 11:25:41 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UkJobMap::Application.config.secret_token = '00433ac4e0596a4bd8bacd78a6469ac2105cec06c7537d6b088a6be6cc773ed3a040a6f85976c7c786380beb3757f2d70bb63f40a62df7de1149cea6e89b4108'
